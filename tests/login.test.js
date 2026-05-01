b0d22210adf89f8b91fbe5406488649ef50b9b23
Marco Realino <marcoarealino@gmail.com>
2026-05-01 20:52:29 -0300

    adicionando o arquivo inicial com teste de performance de login

---
+++ tests/login.test.js
@@ -0,0 +1,40 @@
+import http from 'k6/http'; // importar a biblioteca http do k6
+import { sleep, check } from 'k6'; // importar as funções sleep & check do k6
+
+export const options = {  // declarar a constante 'options' para exportação, visando setar as configurações e critérios do teste 
+  iterations: 50,
+  thresholds: {
+    http_req_duration: ['p(90)<5', 'max<90']
+  },
+}
+
+export default function() { // declarar a função default como exportação e incluir o target URL e 1 segundo de pausa. É aqui que o teste será realizado.
+
+    // criar uma constante para cada informação a ser enviada na request (url, payload, content etc)
+    // incluir checks para validar o funcionamento da API
+  
+
+  const url = 'http://localhost:3000/login'
+
+  const payload = JSON.stringify({
+    username: 'julio.lima',
+    senha: '123456',
+  })
+
+  const params = {
+    headers: {
+      'Content-Type': 'application/json',
+    }
+  }
+
+  const response = http.post(url, payload, params)
+
+  check(response, {
+    'Validar que status é 200': (r) => r.status === 200,
+    'Validar que token é string': (r) => typeof(r.json().token) == 'string'
+  })
+
+  sleep(1);
+
+}
+
