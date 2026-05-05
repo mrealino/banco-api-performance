963c2d4d80496abb780b7afdad275b59f1317038
Marco Realino <marcoarealino@gmail.com>
2026-05-05 20:42:11 -0300

    Adicionando Fixtures

--- tests/login.test.js
+++ tests/login.test.js
@@ -1,9 +1,18 @@
 import http from 'k6/http'; // importar a biblioteca http do k6
 import { sleep, check } from 'k6'; // importar as funções sleep & check do k6
+const postLogin = JSON.parse(open('../fixtures/postLogin.json')) //pega o texto da fixture e transforma em objeto para ser usado na chamada da API
+
 
 export const options = {  // declarar a constante 'options' para exportação, visando setar as configurações e critérios do teste 
-  vus: 10,
-  duration: '30s',
+
+  stages: [
+   { duration: '10s', target: 10 },
+   { duration: '20s', target: 10 },
+   { duration: '10s', target: 30 },
+   { duration: '20s', target: 30 },
+   { duration: '20s', target: 0 }
+  ],
+  
   thresholds: {
     http_req_duration: ['p(90)<3000', 'max<5000'],
     http_req_failed: ['rate<0.01']
@@ -18,10 +27,10 @@ export default function() { // declarar a função default como exportação e i
 
   const url = 'http://localhost:3000/login'
 
-  const payload = JSON.stringify({
-    username: 'julio.lima',
-    senha: '123456',
-  })
+  postLogin.username = "junior.lima" // usado para alterar alguma propriedade do arquivo fixture para o teste
+  console.log(postLogin)
+  
+  const payload = JSON.stringify(postLogin)
 
   const params = {
     headers: {
